35cc7e3593b2ae76cb28ad85e1aabaf564c88145
basantasharmajec <basantasharmajec@gmail.com>
2015-04-17 02:06:06 +0530

    mod

---
+++ new.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2015 at 10:35 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `userID` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=91 ;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userID`, `username`, `message`) VALUES
+(28, 'saiful islam', 'Software Engineer bongeysssdddddda'),
+(29, 'saiful islam', 'Software Engineer bongeysssdddddda'),
+(30, 'saiful islam', 'Software Engineer bongeysssddddddawqw'),
+(31, 'saiful islam', 'Software Engineer bongeysssddddddawqwwqw'),
+(32, 'saiful islam', 'Software Engineer bongeysssddddddawqwwqwww'),
+(35, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(36, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(37, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(38, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(39, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(42, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(43, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(44, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(45, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(46, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(47, 'saiful islam', 'Software Engineer bongeysssddddddawwwwqwwqwww'),
+(49, 'saiful islam', 'Soft'),
+(51, 'saiful islam', 'Soft'),
+(52, 'saiful islam', 'Soft'),
+(53, 'saiful islam', 'Soft'),
+(54, 'saiful islam', 'Soft'),
+(55, 'saiful islam', 'Soft'),
+(56, 'saiful islam', 'Soft'),
+(57, 'saiful islam', 'Soft'),
+(68, 'saifuisddlawwm', 'Soddfdssdt'),
+(69, 'saifuisddlawwm', 'Soddfdssdt'),
+(70, 'sankhadeep', 'happy hoil...'),
+(71, 'sankhadeep', 'happy hoil...'),
+(72, 'sankhadeep', 'happy hoil...'),
+(73, 'sankhadeep', 'happy hoil...'),
+(74, 'sankhadeep', 'happy hoil...'),
+(75, 'sankhadeep', 'happy hoil...'),
+(76, 'sankhadeep', 'happy hoil...'),
+(77, 'sankhadeep', 'happy hoil...'),
+(78, 'sankhadeep', 'happy hoil...'),
+(79, 'sankhadeep', 'happy hoil...'),
+(80, 'sankhadeep', 'happy hoil...'),
+(81, 'sankhadeep', 'happy hoil...'),
+(82, 'sankhadeep', 'happy hoil...'),
+(83, 'sankhadeep', 'happy hoil...'),
+(84, 'dgsgdsgd', 'dsdsd'),
+(85, 'dgsgdsgd', 'dsdsd'),
+(86, 'dgsgdsgd', 'dsdsd'),
+(87, 'dgsgdsgd', 'dsdsd'),
+(88, 'dgsgdsgd', 'dsdsd'),
+(89, 'dgsgdsgd', 'dsdsd'),
+(90, 'dgsgdsgd', 'dsdsd');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
